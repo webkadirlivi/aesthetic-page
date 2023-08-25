afded6a4cec1fb04229dfced2a0af286885780dc
livigames <114938890+webkadirlivi@users.noreply.github.com>
2023-08-25 12:43:14 +0300

    project html page
    adding project.html page

--- path.js
+++ path.js
@@ -1,3 +1,3 @@
-document.querySelector(".projects").addEventListener("click", function() {
+document.getElementById("projectDiv").addEventListener("click", function() {
     window.location.href = "project.html";
 });
